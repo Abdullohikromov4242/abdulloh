0bf09b5827dcf4adc84cf1ddccb15ac64867cec6
Abdullohikromov4242 <abdullohikromov@gmail.com>
2024-05-27 17:20:49 +0500

    Abdulloh07

--- public/js/app.js
+++ public/js/app.js
@@ -44,6 +44,56 @@ imgmain4.addEventListener('click', function () {
     imgmain4.classList.toggle('rotate-180')
    
 })
+let imgmain5 = document.querySelector('.main-img5')
+let salom5 = document.querySelector('.salom5')
+imgmain5.addEventListener('click', function () {
+    salom5.classList.toggle('hidden')
+    imgmain5.classList.toggle('rotate-180')
+   
+})
+let imgmain6 = document.querySelector('.main-img6')
+let salom6 = document.querySelector('.salom6')
+imgmain6.addEventListener('click', function () {
+    salom6.classList.toggle('hidden')
+    imgmain6.classList.toggle('rotate-180')
+   
+})
+let imgmain7 = document.querySelector('.main-img7')
+let salom7 = document.querySelector('.salom7')
+imgmain7.addEventListener('click', function () {
+    salom7.classList.toggle('hidden')
+    imgmain7.classList.toggle('rotate-180')
+   
+})
+let imgmain8 = document.querySelector('.main-img8')
+let salom8 = document.querySelector('.salom8')
+imgmain8.addEventListener('click', function () {
+    salom8.classList.toggle('hidden')
+    imgmain8.classList.toggle('rotate-180')
+   
+})
+let imgmain9 = document.querySelector('.main-img9')
+let salom9 = document.querySelector('.salom9')
+imgmain5.addEventListener('click', function () {
+    salom9.classList.toggle('hidden')
+    imgmain9.classList.toggle('rotate-180')
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
